284ddb8b6dfd52a9816e010624e99019dd641144
Karolis Mazukna <k@karolis.dev>
2024-06-20 23:30:53 +0300

    refactor: Improve message selection algorithm and add support for photos

--- parse.go
+++ parse.go
@@ -21,12 +21,13 @@ type Chat struct {
 }
 
 type Message struct {
-	ID       int         `json:"id"`
-	Type     string      `json:"type"`
-	Text     interface{} `json:"text"`
-	Photo    string      `json:"photo"`
-	From     string      `json:"from"`
-	DateUnix string      `json:"date_unixtime"`
+	ID               int         `json:"id"`
+	Type             string      `json:"type"`
+	Text             interface{} `json:"text"`
+	Photo            string      `json:"photo"`
+	From             string      `json:"from"`
+	DateUnix         string      `json:"date_unixtime"`
+	ReplyToMessageID int         `json:"reply_to_message_id"`
 }
 
 func LastYearBod(t time.Time) int64 {
@@ -62,30 +63,72 @@ func main() {
 	toPeriod := LastYearNextDayBod(now)
 
 	messages := []Message{}
+	reply_index := make(map[int]int)
 
+	// select viable messages for the period
+	// calculate replies for all messages
 	for _, msg := range chat.Messages {
 		msgDate, err := strconv.ParseInt(msg.DateUnix, 10, 64)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		if (msgDate >= fromPeriod) && (msgDate <= toPeriod) && (msg.Text != "") {
-			if str, ok := msg.Text.(string); ok {
-				if len(str) > 4 {
+		if (msgDate >= fromPeriod) && (msgDate <= toPeriod) {
+			if msg.ReplyToMessageID != 0 {
+				val, ok := reply_index[msg.ReplyToMessageID]
+				if !ok {
+					reply_index[msg.ReplyToMessageID] = 0
+				}
+				reply_index[msg.ReplyToMessageID] = val + 1
+			}
+			if msg.Text != "" {
+				if str, ok := msg.Text.(string); ok {
+					if len(str) > 4 {
+						messages = append(messages, msg)
+					}
+				}
+			} else {
+				if msg.Photo != "" {
 					messages = append(messages, msg)
 				}
 			}
 		}
 	}
 
+	messages_map := make(map[int]Message)
+	for _, msg := range messages {
+		messages_map[msg.ID] = msg
+	}
+
 	fmt.Println("Messages a year ago:", len(messages))
 
-	// TODO: implement a better algorithm for selecting message of the day
-	randomMessageIndex := rand.Intn(len(messages))
-	fmt.Println("Random message index:", randomMessageIndex)
+	// find message with most replies
+	max_replies := 0
+	max_replies_id := 0
+	for id, replies := range reply_index {
+		if replies > max_replies && replies > 1 {
+			if _, ok := messages_map[id]; !ok {
+				continue
+			}
+			max_replies = replies
+			max_replies_id = id
+		}
+	}
 
-	randomMessage := messages[randomMessageIndex]
-	fmt.Println("Random message:", randomMessage)
+	var selectedMessage Message
+
+	if max_replies_id == 0 {
+		fmt.Println("Selecting random message")
+		randomMessageIndex := rand.Intn(len(messages))
+		randomMessage := messages[randomMessageIndex]
+		selectedMessage = randomMessage
+	} else {
+		fmt.Println("Selecting message with most replies with replies:", max_replies)
+		most_replies_message := messages_map[max_replies_id]
+		selectedMessage = most_replies_message
+	}
+
+	fmt.Println("Selected message:", selectedMessage)
 
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_KEY"))
 	if err != nil {
@@ -97,21 +140,46 @@ func main() {
 		log.Panic(err)
 	}
 
-	formattedMessage := fmt.Sprintf("MOTD from *%s*:\n%s", randomMessage.From, randomMessage.Text)
+	var formattedMessage string
+
+	if selectedMessage.Text != "" {
+		formattedMessage = fmt.Sprintf("MOTD from *%s*:\n%s", selectedMessage.From, selectedMessage.Text)
+	} else {
+		formattedMessage = fmt.Sprintf("MOTD from *%s*", selectedMessage.From)
+	}
 
 	if os.Getenv("TELEGRAM_DRY_RUN") == "true" {
-		fmt.Println("Dry run, not sending message")
+		fmt.Println("TELEGRAM_DRY_RUN=true, not sending MOTD")
 		fmt.Println(formattedMessage)
 	} else {
-		// TODO: support other message types, include photos
-		msg := tgbotapi.NewMessage(parentChat, formattedMessage)
-		msg.ReplyToMessageID = randomMessage.ID
-		msg.ParseMode = "Markdown"
-		msg.AllowSendingWithoutReply = true
+		if selectedMessage.Photo != "" {
+			photoBytes, err := os.ReadFile("exports/" + selectedMessage.Photo)
+			if err != nil {
+				panic(err)
+			}
+			photoFileBytes := tgbotapi.FileBytes{
+				Name:  "picture",
+				Bytes: photoBytes,
+			}
+			photo := tgbotapi.NewPhoto(parentChat, photoFileBytes)
+			photo.Caption = formattedMessage
+			photo.ParseMode = "Markdown"
+			photo.AllowSendingWithoutReply = true
 
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Panic(err)
+			_, err = bot.Send(photo)
+			if err != nil {
+				log.Panic(err)
+			}
+		} else {
+			msg := tgbotapi.NewMessage(parentChat, formattedMessage)
+			msg.ReplyToMessageID = selectedMessage.ID
+			msg.ParseMode = "Markdown"
+			msg.AllowSendingWithoutReply = true
+
+			_, err = bot.Send(msg)
+			if err != nil {
+				log.Panic(err)
+			}
 		}
 	}
 }
